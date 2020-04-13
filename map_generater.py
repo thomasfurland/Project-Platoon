a868f93b0a3c448c61cd2eb32c58837117078f31
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-12 23:15:40 -0700

    refactor random map and add print map function

--- map_generater.py
+++ map_generater.py
@@ -1,8 +1,14 @@
 import random
 
 def random_map(width, height):
-    return [[
-        random.randint(0, 3) for _ in range(width)
-    ] for _ in range(height)]
+    result = []
+    for _ in range(height):
+        row = [random.randint(0, 3) for _ in range(width)]
+        result.append(row)
+    return result
 
-print(random_map(6, 4))
+def print_map(map):
+    for i in map:
+        print(i)
+
+print_map(random_map(6, 4))
