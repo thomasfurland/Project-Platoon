52ff71739df16b5906c2adf26ae2a980ff346c93
thomas <me@thomas.ca>
2020-04-13 13:51:13 +0900

    merge test

--- map_generater.py
+++ map_generater.py
@@ -6,7 +6,9 @@ class Map:
         self.map_array = []
 
     def generate(self):
-        print(self._shortest)
+        for i in range(self._shortest):
+            self.map_array.append(f"land{self._shortest}")
+        print(self.map_array)
 
 map = Map(3)
 map.generate()
