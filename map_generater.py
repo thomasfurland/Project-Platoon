7af88e90b689c1b7cd0785a704f2bf5bee19d67c
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-12 23:09:09 -0700

    random generated map

--- map_generater.py
+++ map_generater.py
@@ -1,14 +1,8 @@
+import random
 
+def random_map(width, height):
+    return [[
+        random.randint(0, 3) for _ in range(width)
+    ] for _ in range(height)]
 
-class Map:
-    def __init__(self, shortest):
-        self._shortest = shortest
-        self.map_array = []
-
-    def generate(self):
-        for i in range(self._shortest):
-            self.map_array.append(f"land{self._shortest}")
-        print(self.map_array)
-
-map = Map(3)
-map.generate()
+print(random_map(6, 4))
