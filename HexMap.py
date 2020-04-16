7e34069846aa5af3500a6fc6e6109beed7467a9f
Wandering Robot <ej_braun@yahoo.com>
2020-04-16 11:37:11 -0700

    Added super cool stuff to neighbors. Also added a 'ring radius' aspect that allows us to see all neighbors within a certain distance of the center hex-tile

--- HexMap.py
+++ HexMap.py
@@ -22,18 +22,33 @@ class HexMap:
     @classmethod
     def convert_map(cls, data):
         pass
-
-    def cube_neighbour(self, cube):
-        xOffset = [-1,0,1]
-        yOffset = [-1,0,1]
+    
+    #starting at bottom left hex 'dist' rings away from the center cube, iterates right to return hex in that row. does same for row above and repeats
+    def cube_neighbour(self, cube,dist=1):
+        xOffset = range(-dist,dist+1)
+        yOffset = range(-dist,dist+1)
         neighbors = []
         for i in xOffset:
             for j in yOffset:
                 if not(i == 0 and j == 0):
                     k = -(i+j)
-                    if k**2 <= 1:
+                    if abs(k) <= dist:
                         neighbors.append(Cube(cube.x+i,cube.y+j,cube.z+k))
+                        #could insert a bunch of stuff here and it will be preformed on every cube/hex inside the 'dist' ring
         return neighbors
 
 if __name__ == '__main__':
-    x = HexMap(map_dict={(1, 0, -1): 10})
+    x = HexMap()
+    start_tile = Cube(0,0,0)
+    dist = 3
+    tiles = x.cube_neighbour(start_tile,dist)
+    for r in range(1,dist+1):
+        print(f'Ring {r}')
+        print('*'*20)
+        itr = 0
+        for tile in tiles:
+            if max(abs(tile.x),abs(tile.y),abs(tile.z)) == r:
+                print(tile.x,tile.y,tile.z)
+                itr += 1
+        print(f'Tiles in ring {r} = {itr}\n')
+
